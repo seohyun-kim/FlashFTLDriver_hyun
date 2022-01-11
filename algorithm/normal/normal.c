9e442d528dd838cf1017ab1335f9add5d9bcb20a
seohyun-kim <selenium34@inu.ac.kr>
2022-01-11 13:57:15 +0900

    Direct mapping real done!

--- algorithm/normal/normal.c
+++ algorithm/normal/normal.c
@@ -66,18 +66,17 @@ uint32_t normal_set(request *const req){ // WRITE
 	my_req->type=DATAW;
 	my_req->param=(void*)params;
 	memcpy((uint32_t*)&(value->value[4*K*(req->key%4)]), &req->key, sizeof(req->key));
-	//printf("test: value->value  = %d\n", *(uint32_t*)(value->value));
+
 
 
 	if (req->key % 4 == 3) {
-		//req->end_req(req);
-
 		__normal.li->write((req->key)/4, PAGESIZE, value, my_req);//
 		inf_free_valueset(value, FS_MALLOC_W);
 	}else{
 		req->end_req(req);
+		free(params);
+		free(my_req);
 	}
-
 	return 0;
 }
 uint32_t normal_remove(request *const req){
@@ -89,31 +88,28 @@ void *normal_end_req(algo_req* input){
 	request *res=input->parents;
 
 	uint32_t ppa;
-
-		switch (input->type) {
-		case DATAR: //READ
-
-			//printf("%d", *(uint32_t*)res->value->value);
-			ppa = *(uint32_t*)&(res->value->value[4*K*((res->key)%4)]);
-			normal_cnt++;
-			if (normal_cnt > 100) {
-				printf("exit over 100. done!\n");
-				exit(0);
-			}
-
-			printf("lba:%u -> ppa:%u\n", res->key, ppa);
-			if (ppa != res->key) {
-				printf("WRONG!\n");
-				exit(1);
-			}
-			break;
-		case DATAW: //WRITE
-			break;
-		default:
-			printf("normal_end_req__default");
-			exit(1);
-			break;
+	switch (input->type) {
+	case DATAR: //READ
+		ppa = *(uint32_t*)&(res->value->value[4*K*((res->key)%4)]);
+		normal_cnt++;
+		if (normal_cnt > 100) {
+			printf("exit over 100. done!\n");
+			exit(0);
 		}
+
+		printf("lba:%u -> ppa:%u\n", res->key, ppa);
+		if (ppa != res->key) {
+			printf("WRONG!\n");
+			exit(1);
+		}
+		break;
+	case DATAW: //WRITE
+		break;
+	default:
+		printf("normal_end_req__default");
+		exit(1);
+		break;
+	}
 
 	res->end_req(res);
 	free(params);
