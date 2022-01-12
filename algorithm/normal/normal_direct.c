7df6d2257413fae21a4e615c47c2639ff5dfebec
seohyun-kim <selenium34@inu.ac.kr>
2022-01-12 15:36:55 +0900

    Page Mapping setup

--- algorithm/normal/normal_direct.c
+++ algorithm/normal/normal_direct.c
@@ -1,5 +1,6 @@
 
 
+
 //  FTL Direct Mapping
 
 #include <string.h>
@@ -9,8 +10,6 @@
 #include "normal.h"
 #include "../../interface/interface.h"
 #include "../../bench/bench.h"
-
-#include <sys/wait.h>
 
 
 extern MeasureTime mt;
@@ -36,47 +35,48 @@ void normal_destroy (lower_info* li, algorithm *algo){
 }
 
 int normal_cnt=0;
-
+int rem_cnt=0;
 
 uint32_t normal_get(request *const req){ // READ
 	normal_params* params=(normal_params*)malloc(sizeof(normal_params));
-	params->test=-1;
 
 	algo_req *my_req=(algo_req*)malloc(sizeof(algo_req));
 	my_req->parents=req;
-	my_req->end_req=normal_end_req; //end È£Ãâ
+	my_req->end_req=normal_end_req;
 	my_req->param=(void*)params;
 	my_req->type=DATAR;
 
-	__normal.li->read(req->key, PAGESIZE, req->value, my_req);
+	__normal.li->read((req->key)/4, PAGESIZE, req->value, my_req);
 	return 1;
 }
 uint32_t normal_set(request *const req){ // WRITE
 	normal_params* params=(normal_params*)malloc(sizeof(normal_params));
-	params->test=1;
+
 	algo_req *my_req=(algo_req*)malloc(sizeof(algo_req));
 	my_req->parents=req;
-	my_req->end_req=normal_end_req; //end È£Ãâ
+	my_req->end_req=normal_end_req;
 
-	// ÀÓ½Ã ÀúÀå
-	value_set* value = inf_get_valueset(NULL, FS_MALLOC_W, PAGESIZE);
+	static value_set* value;
 
-
-
+	// value buffer
+	if (req->key % 4 == 0) {
+		value = inf_get_valueset(NULL, FS_MALLOC_W, PAGESIZE);
+	}
 
 	my_req->type=DATAW;
 	my_req->param=(void*)params;
+	memcpy((uint32_t*)&(value->value[4*K*(req->key%4)]), &req->key, sizeof(req->key));
 
-	//memcpy(req->value->value, &req->key, sizeof(req->key));
-	memcpy(value, &req->key, sizeof(LPAGESIZE));
 
-	req->end_req(req); 
-	__normal.li->write(req->key, LPAGESIZE, value, my_req);//4KBì”©
-	//__normal.li->write(req->key, PAGESIZE, req->value, my_req);
 
-	//ÀÓ½Ã ÀúÀå °ø°£ free 
-	inf_free_valueset(value, FS_MALLOC_W);
-
+	if (req->key % 4 == 3) {
+		__normal.li->write((req->key)/4, PAGESIZE, value, my_req);//
+		inf_free_valueset(value, FS_MALLOC_W);
+	}else{
+		req->end_req(req);
+		free(params);
+		free(my_req);
+	}
 	return 0;
 }
 uint32_t normal_remove(request *const req){
@@ -86,36 +86,32 @@ uint32_t normal_remove(request *const req){
 void *normal_end_req(algo_req* input){
 	normal_params* params=(normal_params*)(input->param);//
 	request *res=input->parents;
-	//res->end_req(res);
-	
+
 	uint32_t ppa;
-
 	switch (input->type) {
-		case DATAR: //READ
+	case DATAR: //READ
+		ppa = *(uint32_t*)&(res->value->value[4*K*((res->key)%4)]);
+		normal_cnt++;
+		if (normal_cnt > 100) {
+			printf("exit over 100. done!\n");
+			exit(0);
+		}
 
-			//ppa = *(uint32_t*)(res->value->value);
-			
-normal_cnt ++;
-			if(normal_cnt > 100){
-				printf("exit over 100");
-				exit(0);
-			}
-			printf("lba:%u -> ppa:%u\n", res->key, ppa);
-			if (ppa != res->key) {
-				printf("WRONG!\n");
-				exit(1);
-			}
-			break;
-		case DATAW: //WRITE
-			//params->test--;
-			break;
-		default:
+		printf("lba:%u -> ppa:%u\n", res->key, ppa);
+		if (ppa != res->key) {
+			printf("WRONG!\n");
 			exit(1);
-			break;
+		}
+		break;
+	case DATAW: //WRITE
+		break;
+	default:
+		printf("normal_end_req__default");
+		exit(1);
+		break;
 	}
 
-	res -> end_req(res);
-
+	res->end_req(res);
 	free(params);
 	free(input);
 	return NULL;
