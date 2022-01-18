ff223f32833497c6a0c2fb4107dc1f943f7c0d31
seohyun-kim <selenium34@inu.ac.kr>
2022-01-18 15:38:31 +0900

    heap-buffer-overflow

--- algorithm/normal/normal.c
+++ algorithm/normal/normal.c
@@ -32,10 +32,10 @@ static __segment* hyun_segment; //
 
 uint32_t normal_create(lower_info* li, blockmanager* a, algorithm* algo) {
 	map_table = (hyun_map*)calloc(RANGE, sizeof(hyun_map));
-	//static uint32_t cnt_write_req = 0;
 	algo->li = li; //lower_info
 	algo->bm = a; //blockmanager
-	hyun_segment = __normal.bm->get_segment(__normal.bm, BLOCK_ACTIVE); // first get_segment in create
+	hyun_segment = (__segment*)malloc(sizeof(__segment)); 
+	hyun_segment = a->get_segment(__normal.bm, BLOCK_ACTIVE); // first get_segment in create
 
 	return 1;
 }
@@ -66,14 +66,11 @@ uint32_t normal_get(request* const req) { // READ
 uint32_t normal_set(request* const req) { // WRITE
 	normal_params* params = (normal_params*)malloc(sizeof(normal_params));
 	algo_req* my_req = (algo_req*)malloc(sizeof(algo_req));
-	hyun_segment = (__segment*)malloc(sizeof(__segment)); //
 	my_req->parents = req;
 	my_req->end_req = normal_end_req;
 
-
 	if (map_table[req->key].is_lba_re_req == true){ // if same lba re-req
 		__normal.bm->bit_unset(__normal.bm, map_table[req->key].ppa);  // origin mem unset
-		//map_table[req->key].ppa = 0;
 	}
 
 	if (__normal.bm->check_full(hyun_segment)) { // if segment is full
@@ -90,7 +87,7 @@ uint32_t normal_set(request* const req) { // WRITE
 		page_start_addr = __normal.bm->get_page_addr(hyun_segment); 
 	}
 
-	map_table[req->key].ppa = LPAGESIZE* (L2PGAP*page_start_addr +(cnt_write_req % L2PGAP));
+	map_table[req->key].ppa =  LPAGESIZE* (L2PGAP*page_start_addr +(cnt_write_req % L2PGAP));
 	printf("page_start_addr: %d/ ppa : %u \n", page_start_addr, map_table[req->key].ppa);
 
 	params->value_buf = value;
@@ -100,7 +97,7 @@ uint32_t normal_set(request* const req) { // WRITE
 	memcpy((uint32_t*)&(value->value[4 * K * (cnt_write_req % 4)]), &req->key, sizeof(req->key)); //버퍼에 copy
 	__normal.bm->bit_set(__normal.bm, map_table[req->key].ppa);
 	map_table[req->key].is_lba_re_req = true;
-
+	printf("cnt_write_req : %d\n", cnt_write_req);
 	if (cnt_write_req % L2PGAP == 3) { //  모아서 쓰기
 		__normal.li->write((map_table[req->key].ppa) / 4, PAGESIZE, value, my_req);
 	}
@@ -125,12 +122,12 @@ void* normal_end_req(algo_req* input) {
 	switch (input->type) {
 	case DATAR: //READ
 		data = *(uint32_t*)&(res->value->value[4*K*(map_table[res->key].ppa %4)]);
-		//normal_cnt++;
-		//if (normal_cnt > 100) {
-			//printf("exit over 100. done!\n");
-			//free(map_table);
-			//exit(0);
-		//}
+		normal_cnt++;
+		if (normal_cnt > 100) {
+			printf("exit over 100. done!\n");
+			free(map_table);
+			exit(0);
+		}
 		printf("lba:%u -> ppa:%u / data: %u\n", res->key, map_table[res->key].ppa, data);
 		if (data != res->key) {
 			printf("WRONG!\n");
