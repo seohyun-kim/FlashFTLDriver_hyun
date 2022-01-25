8c8e2e3a0852b137a42b293260a1fa3ee5f08151
seohyun-kim <selenium34@inu.ac.kr>
2022-01-25 16:48:22 +0900

    page_map_gc_update  정의

--- algorithm/normal/hyun_gc.c
+++ algorithm/normal/hyun_gc.c
@@ -86,7 +86,7 @@ void travel_page_in_segment(algorithm* __normal, __gsegment* _target_segment, __
 				g_buffer.idx++;
 
 				if (g_buffer.idx == L2PGAP) {
-					uint32_t res = page_map_gc_update(g_buffer.key, L2PGAP);
+					uint32_t res = page_map_gc_update(g_buffer.key, L2PGAP, __normal);
 					validate_ppa(res, g_buffer.key, g_buffer.idx);
 					send_req(res, GCDW, inf_get_valueset(g_buffer.value, FS_MALLOC_W, PAGESIZE));
 					g_buffer.idx = 0;
@@ -162,3 +162,26 @@ void* page_gc_end_req(algo_req* input) {
 	return NULL;
 }
 
+
+uint32_t page_map_gc_update(KEYT* lba, uint32_t idx, algorithm* __normal) {
+	uint32_t res = 0;
+	pm_body* p = (pm_body*)__normal->algo_body;
+
+	/*when the gc phase, It should get a page from the reserved block*/
+	res = __normal->bm->get_page_addr(p->reserve);
+	uint32_t old_ppa, new_ppa;
+	for (uint32_t i = 0; i < idx; i++) {
+		KEYT t_lba = lba[i];
+		if (p->mapping[t_lba] != UINT_MAX) {
+			/*when mapping was updated, the old one is checked as a inavlid*/
+			//invalidate_ppa(p->mapping[t_lba]);
+		}
+		/*mapping update*/
+		p->mapping[t_lba] = res * L2PGAP + i;
+		if (t_lba == test_key) {
+			printf("test_key(%u) is set to %u in gc\n", test_key, res * L2PGAP + i);
+		}
+	}
+
+	return res;
+}
